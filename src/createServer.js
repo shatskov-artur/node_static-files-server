37522e1408132b91fee0bf31aa0d05f5155056f2
Artur Shatskov <artur.shackov77@gmail.com>
2026-02-07 13:10:18 +0100

    Load local changes

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,46 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    if (!normalizedUrl.pathname.startsWith('/file/')) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Should be /file/*');
+
+      return;
+    }
+
+    const filePath = normalizedUrl.pathname.slice(6);
+
+    if (filePath.includes('//')) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File Not Found');
+
+      return;
+    }
+
+    if (filePath.includes('..')) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('File Not Found');
+
+      return;
+    }
+
+    fs.readFile(`public/${filePath}`, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File Not Found');
+
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(data);
+    });
+  });
 }
 
 module.exports = {
