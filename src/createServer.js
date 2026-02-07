fec4001a081baf506758df02d823d1f1b1a9f32c
Artur Shatskov <artur.shackov77@gmail.com>
2026-02-07 15:26:27 +0100

    Solution3

--- src/createServer.js
+++ src/createServer.js
@@ -6,41 +6,31 @@ const fs = require('fs');
 function createServer() {
   return http.createServer((req, res) => {
     const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
-    const { pathname } = normalizedUrl;
 
-    if (pathname.includes('//')) {
+    if (!normalizedUrl.pathname.startsWith('/file/')) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Should be /file/*');
+
+      return;
+    }
+
+    const filePath = normalizedUrl.pathname.slice(6);
+
+    if (filePath.includes('..')) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Bad Request');
+
+      return;
+    }
+
+    if (filePath.includes('//')) {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('File Not Found');
 
       return;
     }
 
-    if (!pathname.startsWith('/file')) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('Should be /file/*');
-
-      return;
-    }
-
-    const relativeFilePath = pathname.replace(/^\/file\/?/, '');
-
-    if (!relativeFilePath) {
-      // Serve index.html when relativeFilePath is empty
-      fs.readFile('public/index.html', (err, data) => {
-        if (err) {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('File Not Found');
-
-          return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(data);
-      });
-
-      return;
-    }
-
-    fs.readFile(`public/${relativeFilePath}`, (err, data) => {
+    fs.readFile(`public/${filePath}`, (err, data) => {
       if (err) {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('File Not Found');
